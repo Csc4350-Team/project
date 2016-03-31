546908d6d1c8ce806f6e59a0291f57f366e66410
jtriplet01 <jtriplet01@gmail.com>
2016-03-31 14:58:38 -0400

    Added files via upload

---
+++ Login.java
@@ -0,0 +1,64 @@
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+public class Login extends Application {
+
+	public static void main (String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) {
+		primaryStage.setTitle("Sahara Login");
+		
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
+		
+		Scene scene = new Scene(grid, 620, 480);
+		primaryStage.setScene(scene);
+		
+		Text scenetitle = new Text("Login to Sahara");
+		scenetitle.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+		grid.add(scenetitle, 0, 0, 2, 1);
+		
+		Label userName = new Label("Email:");
+		grid.add(userName, 0, 1);
+		
+		TextField userTextField = new TextField();
+		grid.add(userTextField, 1, 1);
+		
+		Label pw = new Label("Password:");
+		grid.add(pw, 0, 2);
+		
+		PasswordField pwBox = new PasswordField();
+		grid.add(pwBox, 1, 2);
+		
+		Button btn = new Button("Sign in");
+		btn.setPadding(new Insets(10, 60, 10, 60));
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_CENTER);
+		hbBtn.getChildren().add(btn);
+		grid.add(hbBtn, 1, 4);
+		
+		primaryStage.show();
+		
+	}
+}
