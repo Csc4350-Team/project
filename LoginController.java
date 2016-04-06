76e2b6937d1d4d22ca49efd1e7945bca60dfa556
jtriplet01 <jtriplet01@gmail.com>
2016-04-06 19:11:01 -0400

    Added files via upload

---
+++ LoginController.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+
+import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+public class LoginControl extends VBox {
+	
+	public LoginControl() {
+		FXMLLoader fxmlLoader = new FXMLLoader (getClass().getResource("custom_control.fxml"));
+		fxmlLoader.setRoot(this);
+		fxmlLoader.setController(this);
+		
+		try {
+			fxmlLoader.load();
+		} catch (IOException exception) {
+			throw new RuntimeException(exception);
+		}
+	}
+
+	public String
+
+}
